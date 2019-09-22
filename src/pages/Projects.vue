651c6f6dced1b837b2a21abc0ebc6ca7f567e374
rssrwn <rssrwn@gmail.com>
2019-09-22 23:12:42 +0100

    Add text and layout for edubot project

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -3,16 +3,19 @@
     <h2 class="subTitle">Projects</h2>
     <div id="projects">
       <div class="project">
-        <div class="projectTitleAndText">
-          <h2 class="projectTitle">Edubot</h2>
+        <div class="projectTitle"><h2 class="projectTitle">EduBot</h2></div>
+        <div>
           <div class="projectText">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <p>Key Stage 3 students are now required to learn basic programming concepts, including looping, functions, sorting and searching. However, a lack of fully qualified Computing teachers has made it very difficult for some schools to meet the curriculum requirements. Which means many students are missing out on crucial programming education.</p>
+            <p>We (a team of four) designed EduBot to help solve this problem. After being given a textual introduction to a programming concept students attempt to implement an algorithm using code blocks which will make a robot move around a level. The goal is make EduBot gather all the nuts and bolts in as few moves as possible. Each level provides the student with a new challenge and a new programming concept to master. The platform also provides teachers with the ability to track their students' progress and to give feedback to individual students on each level.</p>
+            <p>EduBot ranked in the top 5 projects in our year group (out of over 40 projects) and our team was invited to present EduBot at the department's open day.</p>
+          </div>
+          <div class="projectPicDiv">
+            <img class="projectPic" src="../assets/edubot.png"/>
           </div>
         </div>
-        <div class="projectPicDiv">
-          <img class="projectPic" src="../assets/Octocat.png"/>
-        </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -35,19 +38,20 @@
   border: 1px lightgrey solid;
   border-radius: 10px;
   padding: 20px;
-  display: flex;
-  justify-content: space-between;
+  display: block;
+  margin: 40px 0px;
 }
 .projectTitle {
   margin: 5px 0px 10px 0px;
 }
 .projectPicDiv {
   width: 100%;
+  display: flex;
+  justify-content: center;
 }
 .projectPic {
-  width: 100%;
-}
-.projectTitleAndText {
-  height: 100%;
+  width: 75%;
+  border: 1px lightgrey solid;
+  border-radius: 5px;
 }
 </style>
