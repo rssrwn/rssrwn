0c65fe7344b51ceb8f07c535ef96b1bd1fdafb00
rssrwn <rssrwn@gmail.com>
2020-07-02 13:38:04 +0100

    Update nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,52 @@
+<!--<template>-->
+  <!--<div id="bar">-->
+    <!--<div id="nav">-->
+      <!--<div class="buttons">-->
+        <!--<div class="circle">-->
+          <!--<div><h3>Home</h3></div>-->
+        <!--</div>-->
+        <!--<div class="circle">-->
+          <!--<div><h3>About</h3></div>-->
+        <!--</div>-->
+      <!--</div>-->
+      <!--<div class="circle large">-->
+        <!--<div id="barTitle">Ross</div>-->
+      <!--</div>-->
+      <!--<div class="buttons">-->
+        <!--<div class="circle">-->
+          <!--<div><h3>Projects</h3></div>-->
+        <!--</div>-->
+        <!--<div class="circle">-->
+          <!--<div><h3>CV</h3></div>-->
+        <!--</div>-->
+      <!--</div>-->
+    <!--</div>-->
+  <!--</div>-->
+<!--</template>-->
+
 <template>
   <div id="bar">
     <div id="nav">
-      <div class="buttons">
-        <div><h3>Home</h3></div>
-        <div><h3>About</h3></div>
+      <div id="mainButton" class="circle large">
+        <div id="barTitle">Ross</div>
       </div>
-      <div id="barTitle">Ross</div>
-      <div class="buttons">
-        <div><h3>Projects</h3></div>
-        <div><h3>CV</h3></div>
+      <div id="childNodes">
+        <div class="buttons left">
+          <div class="circle">
+            <div>Home</div>
+          </div>
+          <div class="circle">
+            <div>About</div>
+          </div>
+        </div>
+        <div class="buttons right">
+          <div class="circle">
+            <div>Projects</div>
+          </div>
+          <div class="circle">
+            <div>CV</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -22,35 +60,67 @@
 
 <style scoped>
 #bar {
-  height: 100px;
   width: available;
   background-color: white;
   display: flex;
   align-items: center;
-  border-bottom: 1px lightgrey solid;
 }
+
 #nav {
-  display: flex;
+  display: inline;
   width: 100%;
-  justify-content: space-evenly;
+  margin-top: -100px;
+  margin-left: 100px;
+  margin-right: 100px;
 }
-.buttons {
-  width: 20%;
+
+#mainButton {
+  margin: auto;
+}
+
+#childNodes {
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
   color: #2c3e50;
-  font-family: 'Merriweather', serif;
-  font-size: 18px;
-  font-weight: bold;
+  font-family: Oswald, sans-serif;
+  font-size: 28px;
+  width:100%;
+  margin-top: -80px;
 }
+
+.buttons {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-evenly;
+  /*margin: 0px 80px;*/
+}
+.buttons.left {
+  margin-right: 120px;
+}
+.buttons.right {
+  margin-left: 120px;
+}
+
+.circle {
+  height: 150px;
+  width: 150px;
+  border-radius: 50%;
+  border: 3px solid #2c3e50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.circle.large {
+  height: 300px;
+  width: 300px;
+}
+
 #barTitle {
   font-family: 'Lobster Two', cursive;
   color: palevioletred;
   font-size: 60px;
   font-weight: bold;
-  text-align: center;
-  justify-content: center;
-  line-height: 100px;
 }
 </style>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -7,7 +7,7 @@
         <div>
           <div class="projectText">
             <p>Key Stage 3 students are now required to learn basic programming concepts, including looping, functions, sorting and searching. However, a lack of fully qualified Computing teachers has made it very difficult for some schools to meet the curriculum requirements. Which means many students are missing out on crucial programming education.</p>
-            <p>We (a team of four) designed EduBot to help solve this problem. After being given a textual introduction to a programming concept students attempt to implement an algorithm using code blocks which will make a robot move around a level. The goal is make EduBot gather all the nuts and bolts in as few moves as possible. Each level provides the student with a new challenge and a new programming concept to master. The platform also provides teachers with the ability to track their students' progress and to give feedback to individual students on each level.</p>
+            <p>We (a team of four) designed EduBot to help solve this problem. After being given a textual introduction to a programming concept, students attempt to implement an algorithm using code blocks which will make a robot move around a level. The goal is make EduBot gather all the nuts and bolts in as few moves as possible. Each level provides the student with a new challenge and a new programming concept to master. The platform also provides teachers with the ability to track their students' progress and to give feedback to individual students on each level.</p>
             <p>EduBot ranked in the top 5 projects in our year group (out of over 40 projects) and our team was invited to present EduBot at the department's open day.</p>
           </div>
           <div class="projectPicDiv">
@@ -43,22 +43,26 @@
 
 <style scoped>
 .subTitle {
-  font-family: 'Merriweather', serif;
+  /*font-family: 'Merriweather', serif;*/
+  font-family: Oswald, sans-serif;
   font-size: 30px;
   font-weight: bold;
   border-bottom: 1px lightgrey solid;
   color: palevioletred;
 }
 .project {
-  font-family: 'Merriweather', serif;
+  /*font-family: 'Merriweather', serif;*/
   border: 1px lightgrey solid;
+  font-family: Nunito, sans-serif;
   border-radius: 10px;
   padding: 20px;
   display: block;
   margin: 40px 0px;
 }
 .projectTitle {
+  font-family: Oswald, sans-serif;
   margin: 5px 0px 10px 0px;
+  font-size: 28px;
 }
 .projectPicDiv {
   padding-top: 10px;
